7cae4e988d60afc8271c52f2a4ea60c8506f1558
Ian <ipatrickenn7@gmail.com>
2023-02-03 14:09:36 -0700

    Vehicle types render works; final dropdown logic needs fixing

---
+++ src/DisplayVehicles.js
@@ -0,0 +1,39 @@
+import { v4 as uuidv4 } from "uuid";
+
+
+function DisplayVehicles({vehicles, gothereby}) {
+
+    // console.log('in display vehicles:', vehicles)
+
+    // vehicles.forEach(vehicle => {
+    //     console.log('each vehicle:', vehicle)
+    // })
+
+    console.log('gothereby:', gothereby)
+
+    return (
+        <>
+        {vehicles.map((vehicle) => (
+            <div id="each_vehicle" key={uuidv4()}>
+              <strong style={{display: "block"}}>{vehicle.manufacturer_name} {vehicle.model}</strong>
+              <img src={vehicle.photo_url} alt="vehicle_photo" /><p style={{display: "inline"}}></p>
+              <strong style={{display: "block"}}>Alternative Fuel Economy (Combined)</strong><p style={{display: "inline"}}>{vehicle.alternative_fuel_economy_combined} MPGe</p>
+              <strong style={{display: "block"}}>Electric-Only Range:</strong><p style={{display: "inline"}}>{vehicle.electric_range}</p>
+              {vehicle.charging_rate_level_2 ? 
+                <><strong style={{display: "block"}}>Charging Rate:</strong><p style={{display: "inline"}}><em>Level 2: </em>{vehicle.charging_rate_level_2} | <em>DC Fast: </em>{vehicle.charging_rate_dc_fast}</p></>
+              : null}
+              {vehicle.charging_speed_level_1 ? 
+                <><strong style={{display: "block"}}>Charging Speed (per hour of charging):</strong><p style={{display: "inline"}}><em>Level 1: </em>{vehicle.charging_speed_level_1} | <em>Level 2: </em>{vehicle.charging_speed_level_2} | <em>DC Fast: </em>{vehicle.charging_speed_dc_fast}</p></>
+              : null}
+                <strong style={{display: "block"}}>Battery Capacity: </strong><p style={{display: "inline"}}>{vehicle.battery_capacity_kwh} </p>
+                <strong style={{display: "block"}}>Engine/Motor(s): </strong><p style={{display: "inline"}}>{vehicle.engine_size}</p>
+                <strong style={{display: "block"}}>Drivetrain: </strong><p style={{display: "inline"}}>{vehicle.drivetrain}</p>
+              <br/>
+            </div>
+          ))
+        }
+        </>
+    )
+}
+
+export default DisplayVehicles;
--- src/EV.js
+++ src/EV.js
@@ -2,18 +2,18 @@ import { useState, useEffect } from "react";
 import sedan from "./Images/sedan.jpeg";
 import suv from "./Images/suv.jpeg";
 import pickup from "./Images/pickup.jpeg";
+import DisplayVehicles from "./DisplayVehicles";
 import { v4 as uuidv4 } from "uuid";
 
 const APIkey = `FWa7ECkCD7pV8HgIVkhHEKXfvBTbeo9ZI6bhRRY5`;
 
 function EV() {
   const [vehicles, setVehicles] = useState([]);
+  const [typeSelected, setTypeSelected] = useState(false);
+  const [dropdownsFulfilled, setDropdownsFulfilled] = useState(false)
 
   const fetchVehicles = (e, vehicleType, year = 2023) => {
     e.preventDefault();
-
-    document.getElementById("vehicle_type_container").id =
-      "vehicle_display_container";
 
     fetch(
       `https://developer.nrel.gov/api/vehicles/v1/light_duty_automobiles.json?api_key=${APIkey}&current=false&fuel_id=41&category_id=${vehicleType}&model_year=${year}`
@@ -28,126 +28,151 @@ function EV() {
         });
       }
     });
+
+    if (e.target.alt === "vehicle_type_icon") {
+      document.getElementById("vehicle_type_container").id =
+      "vehicle_display_container";
+      setTypeSelected(true)
+    }
   };
 
-  const extraVehicleInfo = [
-    {
-    id: 13221,
-    model: "e-tron GT/RS e-tron GT",
-    msrp: "$104,900",
-  },
-]
+//   const extraVehicleInfo = [
+//     {
+//     id: 13221,
+//     model: "e-tron GT/RS e-tron GT",
+//     msrp: "$104,900",
+//   },
+// ]
 
+  const makesByYear = [];
   for (let i = 0; i < vehicles.length; i++) {
-    // console.log('vehicles[i].id:', vehicles[i].id)
-    // console.log('extraVehicleInfo[i].id:', extraVehicleInfo[i].id)
-    if (extraVehicleInfo[i].id) {
-      if ((vehicles[i].id === extraVehicleInfo[i].id) && (!vehicles[i].msrp)) {
-        vehicles[i].msrp = extraVehicleInfo[i].msrp
-        }
+    if (vehicles[i].manufacturer_name && (!makesByYear.includes(vehicles[i].manufacturer_name))) {
+      makesByYear.push(vehicles[i].manufacturer_name);
     }
-    console.log(vehicles[0])
-    }
+  }
+  
+  const modelsByMake = [];
 
-  const goBack = (e) => {
+  const selectModel = (e) => {
     e.preventDefault()
 
+    const modelsByMake = [];
+    for (let i = 0; i < vehicles.length; i++) {
+      if ((vehicles[i].manufacturer_name === e.target.value) && !modelsByMake.includes(vehicles[i].manufacturer_name)) {
+        modelsByMake.push(vehicles[i].model)
+      }
+    }
+    return modelsByMake.map(make => {
+      <option key={uuidv4()} value={make}>{make}</option>
+    })
+  }
+  
+  const goBack = (e) => {
+    e.preventDefault()
+  
+    if (document.getElementById("vehicle_display_container"))
     document.getElementById("vehicle_display_container").id =
     "vehicle_type_container";
-
+  
     setVehicles([])
+    setDropdownsFulfilled(false)
+    setTypeSelected(false)
   }
-
 
   return (
     <>
-    <button onClick={goBack}>go back</button>
-      {!vehicles.length ? (
+      <button onClick={goBack}>go back</button>
+      {!dropdownsFulfilled && !typeSelected ? (
         <>
           <div>
             <h4>Search by specific vehicle</h4>
+{/* ===================================================================== */}
             <label htmlFor="automobiles">Model year: </label>
+
             <select
               name="automobiles"
               id="automobiles"
               form="automobile_form"
-              onChange={fetchVehicles}
-            >
+              onChange={(e) => fetchVehicles(e, `27,29,25`, e.target.value)}>
+
               <option>select</option>
-              <option value={"2023"}>2023</option>
-              <option value={"2022"}>2022</option>
-              <option value={"2021"}>2021</option>
-            </select>
 
+              <option value={2023}>2023</option>
+              <option value={2022}>2022</option>
+              <option value={2021}>2021</option>
+
+            </select>
+{/* ===================================================================== */}
             <label htmlFor="automobiles">Make: </label>
+            
             <select
               name="automobiles"
               id="automobiles"
               form="automobile_form"
-              onChange={fetchVehicles}
-            >
-              <option value={"2023"}>2023</option>
-              <option value={"2022"}>2022</option>
-              <option value={"2021"}>2021</option>
-            </select>
+              onChange={selectModel}>
 
-            <label htmlFor="automobiles">Model: </label>
-            <select
-              name="automobiles"
-              id="automobiles"
-              form="automobile_form"
-              onChange={fetchVehicles}
-            >
-              <option value={"2023"}>2023</option>
-              <option value={"2022"}>2022</option>
-              <option value={"2021"}>2021</option>
+              <option>select</option>
+
+              {makesByYear.length
+                ? {selectModel}
+                : null}
             </select>
-          </div>
+{/* ===================================================================== */}
+            <label htmlFor="automobiles">Model: </label>
+
+            <select name="automobiles" id="automobiles" form="automobile_form">
+
+              <option>select</option>
+            </select>
+{/* ===================================================================== */}
+
+
+
+
+
+
+
+            </div>
           <div></div>
         </>
       ) : null}
+      {dropdownsFulfilled ? <DisplayVehicles gothereby={"dropdowns"} /> : null}
 
       <br />
       <br />
 
       <div>
-        <h4>Search by type</h4>
-
         <div id="vehicle_type_container">
-          {!vehicles.length ? (
+          {!typeSelected ? (
             <>
+              <h4>Search by type</h4>
               <div onClick={(e) => fetchVehicles(e, 27)}>
                 <h5>Sedan/ Wagon</h5>
-                <img style={{ width: "40%" }} src={sedan} alt="sedan" />
+                <img
+                  style={{ width: "40%" }}
+                  src={sedan}
+                  alt="vehicle_type_icon"
+                />
               </div>
               <div onClick={(e) => fetchVehicles(e, 29)}>
                 <h5>SUV</h5>
-                <img style={{ width: "40%" }} src={suv} alt="sedan" />
+                <img
+                  style={{ width: "40%" }}
+                  src={suv}
+                  alt="vehicle_type_icon"
+                />
               </div>
               <div onClick={(e) => fetchVehicles(e, 25)}>
                 <h5>Pickup</h5>
-                <img style={{ width: "40%" }} src={pickup} alt="sedan" />
+                <img
+                  style={{ width: "40%" }}
+                  src={pickup}
+                  alt="vehicle_type_icon"
+                />
               </div>
             </>
           ) : (
-            vehicles.map((vehicle) => (
-              <div id="each_vehicle" key={uuidv4()}>
-                <strong style={{display: "block"}}>{vehicle.manufacturer_name} {vehicle.model}</strong>
-                <img src={vehicle.photo_url} alt="vehicle_photo" /><p style={{display: "inline"}}></p>
-                <strong style={{display: "block"}}>Alternative Fuel Economy (Combined)</strong><p style={{display: "inline"}}>{vehicle.alternative_fuel_economy_combined} MPGe</p>
-                <strong style={{display: "block"}}>Electric-Only Range:</strong><p style={{display: "inline"}}>{vehicle.electric_range}</p>
-                {vehicle.charging_rate_level_2 ? 
-                  <><strong style={{display: "block"}}>Charging Rate:</strong><p style={{display: "inline"}}><em>Level 2: </em>{vehicle.charging_rate_level_2} | <em>DC Fast: </em>{vehicle.charging_rate_dc_fast}</p></>
-                : null}
-                {vehicle.charging_speed_level_1 ? 
-                  <><strong style={{display: "block"}}>Charging Speed (per hour of charging):</strong><p style={{display: "inline"}}><em>Level 1: </em>{vehicle.charging_speed_level_1} | <em>Level 2: </em>{vehicle.charging_speed_level_2} | <em>DC Fast: </em>{vehicle.charging_speed_dc_fast}</p></>
-                : null}
-                  <strong style={{display: "block"}}>Battery Capacity: </strong><p style={{display: "inline"}}>{vehicle.battery_capacity_kwh} </p>
-                  <strong style={{display: "block"}}>Engine/Motor(s): </strong><p style={{display: "inline"}}>{vehicle.engine_size}</p>
-                  <strong style={{display: "block"}}>Drivetrain: </strong><p style={{display: "inline"}}>{vehicle.drivetrain}</p>
-                <br/>
-              </div>
-            ))
+            <DisplayVehicles gothereby={"types"} vehicles={vehicles} />
           )}
         </div>
       </div>
